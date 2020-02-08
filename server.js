720e65088f4bfc4df976e94d4b1e113ee2d595f5
Chris-Warren <chriswarren_t@hotmail.com>
2020-02-08 14:29:23 +0000

    due to bug with git hub trying to re commit and push changes I made to the server.js file

--- server.js
+++ server.js
@@ -1,47 +1,49 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const MongoClient = require("mongodb").MongoClient;
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const MongoClient = require('mongodb').MongoClient;
+//used to get rid of cors error and access-control-allow-origin error
+const cors = require('cors')
 let db;
-//connecting to the mongoDB database
-MongoClient.connect("mongodb://localhost:27017", (err, client) => {
-  db = client.db("web");
-});
-//middleware
-app.use(bodyParser.json());
-//------
 
-//api get
-app.get("/", (req, res, next) => {
-  res.send("Select a collection, e.g., /collections/messages");
-});
+app.use(cors())
+app.use(bodyParser.json());
+
+//connecting to the mongodb using nodejs driver
+MongoClient.connect('mongodb://localhost:27017', (err, client) =>{
+    db = client.db('web')
+})
+
+app.get('/', (req, res, next) => {
+    res.send('Select a collection, e.g., /collections/messages')
+    }
+)
 
 //lets you get the collection youre requesting
-app.param("collectionName", (req, res, next, collectionName) => {
-  req.collection = db.collection(collectionName);
-  return next();
-});
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    return next()
+    })
+
 
 //getting the collection and displaying all its content
-app.get("/collections/:collectionName", (req, res, next) => {
-  req.collection.find({}).toArray((e, results) => {
-    if (e) return next(e);
-    res.send(results);
-  });
-});
+app.get('/collections/:collectionName', (req, res, next) => {
+    //projection excludes ID from being shown
+    req.collection.find({}, {projection: {_id: 0}}).toArray((e, results) => {
+        if (e) return next(e)
+        res.send(results)
+    }) 
+})
 
-app.post("/collections/:collectionName", (req, res, next) => {
-//   req.collection.insert({
-//     email: req.body.email,
-//     password: req.body.password,
-//     type: req.body.type
-//   });
-    console.log(req);
-  res.send({ok: 'accepted'});
-});
 
-//opening server on port 3000
+//Storing user and course info to the mongoDB
+app.post('/collections/:collectionName', (req, res, next) => {
+    req.collection.insert(req.body, (err, results) => {
+        if (err) return next(err)
+        res.send(results.ops)
+    })
+})
+  
 app.listen(3000, () => {
-  console.log("Open at localhost:3000");
-});
+    console.log('Open at localhost:3000')
+    })
